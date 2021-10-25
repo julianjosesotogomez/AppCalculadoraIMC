36f8471d4d60037fcd95e2ffe3942c343c523a3d
julianjosesotogomez <76453216+julianjosesotogomez@users.noreply.github.com>
2021-10-25 08:31:11 -0500

    App para calcular IMC

--- AppCalculadoraIMC/AppCalculadoraIMC/MainPage.xaml.cs
+++ AppCalculadoraIMC/AppCalculadoraIMC/MainPage.xaml.cs
@@ -14,5 +14,42 @@ namespace AppCalculadoraIMC
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Altura.Text) && !string.IsNullOrEmpty(Peso.Text))
+            {
+                var altura = double.Parse(Altura.Text);
+                var peso = double.Parse(Peso.Text);
+
+                var imc = peso / (altura * altura);
+
+                IMC.Text = imc.ToString();
+
+                string resultado = "";
+
+                if (imc < 18.5)
+                {
+                    resultado = "Tienes bajo peso";
+                }
+                else if (imc >= 18.5 && imc <= 24.9)
+                {
+                    resultado = "Tienes peso normal";
+                }
+                else if (imc >= 25 && imc <= 29.9)
+                {
+                    resultado = "Tienes sobrepeso";
+                }
+                else
+                {
+                    resultado = "Tienes Obesidad, Cuidate!";
+                }
+                DisplayAlert("Resultado", resultado, "OK");
+            }
+            else
+            {
+                DisplayAlert("Datos erróneos", "Por favor, llena toda la informacion", "OK");
+            }
+        }
     }
 }
